70cdf86936b9a44c76dd815100775072586bbaec
D3vlin <alexisdq16@gmail.com>
2025-06-03 18:36:35 -0500

    initial commit

---
+++ src/main/java/com/d3vlin13/market/MarketApplication.java
@@ -0,0 +1,13 @@
+package com.d3vlin13.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketApplication.class, args);
+	}
+
+}
