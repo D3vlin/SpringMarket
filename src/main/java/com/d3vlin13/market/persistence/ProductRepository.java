3140150aee7052b63c4477c548a05e7f74b99c76
D3vlin <alexisdq16@gmail.com>
2025-06-04 07:55:26 -0500

    added query methods

--- src/main/java/com/d3vlin13/market/persistence/ProductRepository.java
+++ src/main/java/com/d3vlin13/market/persistence/ProductRepository.java
@@ -2,13 +2,36 @@ package com.d3vlin13.market.persistence;
 
 import com.d3vlin13.market.persistence.crud.IProductCrudRepository;
 import com.d3vlin13.market.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductRepository {
     private IProductCrudRepository productCrudRepository;
 
     public List<Product> findAll() {
         return (List<Product>) productCrudRepository.findAll();
+    }
+
+    public List<Product> getByCategory(int idCategory) {
+        return productCrudRepository.findByIdCategoryOrderByNameAsc(idCategory);
+    }
+
+    public Optional<List<Product>> getLessThan(int stockQuantity) {
+        return productCrudRepository.findByStockQuantityLessThanAndState(stockQuantity, true);
+    }
+
+    public Optional<Product> findById(int idProduct) {
+        return productCrudRepository.findById(idProduct);
+    }
+
+    public Product save(Product product) {
+        return productCrudRepository.save(product);
+    }
+
+    public void deleteById(int idProduct) {
+        productCrudRepository.deleteById(idProduct);
     }
 }
--- src/main/java/com/d3vlin13/market/persistence/crud/IProductCrudRepository.java
+++ src/main/java/com/d3vlin13/market/persistence/crud/IProductCrudRepository.java
@@ -3,6 +3,10 @@ package com.d3vlin13.market.persistence.crud;
 import com.d3vlin13.market.persistence.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface IProductCrudRepository extends CrudRepository<Product, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface IProductCrudRepository extends CrudRepository<Product, Integer> {
+    List<Product> findByIdCategoryOrderByNameAsc(int idCategory);
+    Optional<List<Product>> findByStockQuantityLessThanAndState(int stockQuantity, Boolean state);
 }
